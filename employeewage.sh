28e41253dcd583c7d7f2e0af01693781a65062e0
Pranil Kakade <pranilkakade111@gmail.com>
2021-01-22 09:44:25 +0530

    calculating daily employee wages

--- employeewage.sh
+++ employeewage.sh
@@ -8,3 +8,17 @@ then
 else
      echo "employee is abscent";
 fi
+
+function daily_wage()
+{
+  isPresent=1;
+randomCheck=$((RANDOM%2));
+if [ $isPresent -eq $randomCheck ];
+then 
+     empRatePerHr=20;
+     empHrs=8;
+     salary=$(($empHrs*$empRatePerHr));
+else
+     salary=0;
+fi
+}
