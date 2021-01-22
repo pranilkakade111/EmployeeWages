62a50b2ab555702e61c4e13169de350b6ffb3abc
Pranil Kakade <pranilkakade111@gmail.com>
2021-01-22 09:50:55 +0530

    Adding part time employee wage

--- employeewage.sh
+++ employeewage.sh
@@ -22,3 +22,20 @@ else
      salary=0;
 fi
 }
+
+function parttime()
+{
+ isPartTime=1;
+isFullTime=2;
+empRatePerHr=20;
+randomCheck=$((RANDOM%3));
+
+if [ $isFulltTime -eq $randomCheck ];
+then
+    empHrs=4;
+else
+     empHrs=0;
+fi
+
+salary=$(($empHrs*$empRatePerHr));
+}
